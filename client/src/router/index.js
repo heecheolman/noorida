9749b76e0deadc615163df86b30f098a990d7d13
heecheolman <msn9975@gmail.com>
2019-01-20 15:22:41 +0900

    client setting

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,8 +1,13 @@
 import Vue from 'vue';
+import Vuesax from 'vuesax';
 import Router from 'vue-router';
 import HelloWorld from '@/components/HelloWorld';
 
+import 'vuesax/dist/vuesax.css';
+import 'material-icons/iconfont/material-icons.css';
+
 Vue.use(Router);
+Vue.use(Vuesax);
 
 export default new Router({
   routes: [

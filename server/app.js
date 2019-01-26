8a2741727d32920b9de294a15d0ac7dc67d1d115
heecheol <msn9975@gmail.com>
2019-01-27 02:15:44 +0900

    feat(client, server): 이메일 토큰인증 구현

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = require('./route/index');
-const path = require('path');
+const index = require('./route/index');
+const cert = require('./route/cert');
 
 const app = express();
+
+const path = require('path');
 
 const PORT = 3000;
 const DIST_DIR = './../client/dist';
@@ -11,22 +13,19 @@ const DIST_DIR = './../client/dist';
 /* 미들웨어 등록 */
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api', router);
+
+app.use('/api', index);
+app.use('/api/cert', cert);
 
 app.use('/static', express.static(path.resolve(__dirname, `${DIST_DIR}/static`)));
 
 /* 기본 라우팅 */
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, `${DIST_DIR}/index.html`));
+  res.sendFile(path.resolve(__dirname, `${DIST_DIR}/index.html`));
 });
 
 /* 3000번 포트 */
 app.listen(PORT, () => {
-    console.log('SERVER START!!!')
-
-    require('./models').sequelize.sync({force: true})
-        .then(() => {
-            console.log('Databases sync');
-        });
+  console.log('SERVER START!!!');
 });
 

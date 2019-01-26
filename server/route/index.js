ad665b4cb1cfc52d049a313747e1a0102e48ba6b
ahkong <devahgkong@google.com>
2019-01-26 19:32:18 +0900

    feat(server): 디비 세팅

--- server/route/index.js
+++ server/route/index.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
+const joinService = require('../service/join');
+const tokenBuilder = require('../util/token-builder');
 
 const router = express.Router();
 
-router.get('/members', (req, res) => {
-  const members = [
-    { id: 1, name: 'heecheol' },
-    { id: 2, name: 'hoocheol' },
-  ];
-  res.json(members);
-});
 
-router.post('/send-mail', (req, res) => {
-  const email = req.body.email;
+router.post('/user', async (req, res) => {
+  // 회원가입
+  const token = tokenBuilder(); // 토큰 생성
 
+  const result = await joinService.insertUser({
+    nickname: req.body.nickname,
+    password: req.body.password,
+    email: req.body.email,
+    token,
+  });
+
+  if (result) {
+    res.sendStatus(200);
+    res.send('ok');
+  } else {
+    res.sendStatus(500);
+  }
+  // 1. 회원가입
+  // 2. 토큰도 같이 insert
+
+  // 이메일 보내는 로직
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -23,11 +36,16 @@ router.post('/send-mail', (req, res) => {
   });
 
   const mailOptions = {
-    from: 'heecheol.bot@gmail.com',
-    to: email,
-    subject: '안녕하세요 누리다입니다.',
-    html: `<p>안녕하세요 누리다입니다. 아래의 링크를 클릭해주세요.</p>
-            <a href="http://localhost:3000/api/auth/?email=$${email}&token=abcdefg">인증하기</a>`,
+    from: {
+      name: '누리다',
+      address: 'heecheol.bot@gmail.com',
+    },
+    to: {
+      address: req.body.email,
+    },
+    subject: '안녕하세요 누리다입니다. 이메일 인증을 해주세요',
+    html: `<p>안녕하세요 누리다입니다. 인증 코드는 다음과 같습니다.</p>
+            <a href="http://localhost:3000/api/auth/?email=$${req.body.email}&token=${token}">인증하기</a>`,
   };
 
   transporter.sendMail(mailOptions, (err, info) => {
@@ -37,9 +55,8 @@ router.post('/send-mail', (req, res) => {
       console.log(`Email sent ${info.response}`);
     }
   });
-
-  res.send('ok');
 });
+
 
 router.get('/auth', (req, res) => {
   console.log('auth get');
@@ -49,12 +66,20 @@ router.get('/auth', (req, res) => {
   console.log('email is', email);
   console.log('token is', token);
 
-  if (token === 'abcdefg') {
-    console.log('join success');
+
+  if (joinService.compareToken()) {
+    // success
   } else {
-    console.log('join failed');
+    // failed
   }
 
+  if (token === 'abcdefg') {
+    console.log('join success');
+    // active true
+  } else {
+    console.log('join failed');
+    // active false
+  }
   res.send('ok');
 });
 

97ccbb1d81cfffeddf1cd47ea2a90018699fd14f
heecheol <msn9975@gmail.com>
2019-01-29 23:05:14 +0900

    feat(server): 인증메일 전송 리팩토링과 디비 쿼리문 최적화

--- server/route/cert.js
+++ server/route/cert.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const tokenBuilder = require('uuid/v4');
-const nodemailer = require('nodemailer');
+const PostOffice = require('./../mail-config/mail-config');
+const certService = require('./../service/cert');
 
 const router = express.Router();
 
@@ -11,57 +12,14 @@ const container = [
   },
 ];
 
-router.post('/mail', (req, res) => {
+router.post('/mail', async (req, res) => {
   const requestEmail = req.body.email;
   const token = tokenBuilder(); // 토큰생성
-  let existUser = false; // 유저가 존재하는지 여부
 
-  // container 돌면서 일치하는 email 이 있는지 확인하고 있으면 토큰을 덮어씌워줌
-  container.forEach((tempUser) => {
-    if (tempUser.email === requestEmail) {
-      existUser = true;
-      tempUser.token = token;
-    }
-  });
+  await certService.upsertUserToken({ tmpEmail: requestEmail, tmpToken: token })
+    .catch(error => error);
 
-  // 만약에 유저가 없으면 새로 생성
-  if (!existUser) {
-    container.push({
-      email: requestEmail,
-      token,
-    });
-  }
-
-  // 이메일 보내기
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-      user: 'heecheol.bot@gmail.com',
-      pass: 'gmlcjf12',
-    },
-  });
-
-  const mailOptions = {
-    from: {
-      name: '누리다',
-      address: 'heecheol.bot@gmail.com',
-    },
-    to: {
-      address: requestEmail,
-    },
-    subject: '안녕하세요 누리다입니다. 이메일 인증을 해주세요',
-    html: `<p>안녕하세요 누리다입니다. 인증 코드는 다음과 같습니다.</p>
-            <p>${token}</p>`,
-  };
-
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      console.error(err);
-    } else {
-      console.log(`Email sent ${info.response}`);
-    }
-  });
-  console.log('sending mail token is', token);
+  PostOffice.transporter.sendMail(PostOffice.mailOptionBuilder(requestEmail, token), PostOffice.mailErrorHandler);
 });
 
 router.get('/compare/:email/:token', (req, res) => {

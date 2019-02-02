d57f8a67fd4ed3999d2792b67196665e6813e480
heecheol <msn9975@gmail.com>
2019-02-03 00:54:00 +0900

    fix(client): ant design 컴포넌트라이브러리 사용

--- client/src/pages/JoinPage.vue
+++ client/src/pages/JoinPage.vue
@@ -2,92 +2,137 @@
   <div class="page flex-container flex-center-sort">
     <div class="join-box">
       <h1 class="title text-center">회원가입</h1>
-        <div class="input-box flex-container flex-center-sort flex-column">
-          <div class="input-item-wrap">
-            <label
-              :class="isTypedLabel"
-              class="label--base">{{ realNamePlaceholder }}</label>
-            <a-input
-              type="text"
-              name="realName"
-              v-model="realName"
-              v-validate.initial="{
-                required: true,
-                regex: /^[가-힝]{2,}$/,
-              }"
-            />
-          </div>
-          <div class="input-item-wrap flex-container flex-center-sort flex-column">
-            <a-input
-                type="text"
+      <div class="input-box flex-container flex-center-sort flex-column">
+        <div class="input-item-wrap flex-container flex-center-sort flex-column">
+          <a-form
+            :form="form"
+            @submit="handleSubmit"
+          >
+            <a-form-item
+              hasFeedback
+            >
+              <a-input
+                placeholder="이름"
+                v-decorator="[
+                    'realName',
+                    {
+                      rules: [{
+                          required: true,
+                          message: '이름을 입력해주세요',
+                        }, {
+                          pattern: /^[가-힝]{2,}$/,
+                          message: '유효하지 않은 이름이에요',
+                      }]
+                    }
+                 ]" />
+            </a-form-item>
+            <a-form-item
+              hasFeedback
+              validateStatus="validating"
+            >
+              <a-input
                 placeholder="닉네임"
-                name="nickName"
-                v-model="nickName"
-                v-validate.initial="{
-                  required: true,
-                  regex: /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{5,21}$/
-                }"
-              />
-          </div>
-          <div class="input-item-wrap flex-container flex-center-sort flex-column">
-            <a-input
-                type="password"
+                v-decorator="[
+                    'nickName',
+                    {
+                      rules: [{
+                          required: true,
+                          message: '닉네임을 입력해주세요',
+                        }, {
+                          pattern: /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{5,21}$/,
+                          message: '유효하지 않은 닉네임이에요',
+                      }]
+                    }
+                  ]"/>
+            </a-form-item>
+            <a-form-item
+              hasFeedback
+            >
+              <a-input
                 placeholder="비밀번호"
-                ref="password"
-                name="password"
-                v-model="password"
-                v-validate.initial="{
-                  required: true,
-                  regex: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{10,}$/
-                }"
-              />
-          </div>
-          <div class="input-item-wrap flex-container flex-center-sort flex-column">
-            <a-input
+                v-decorator="[
+                    'password',
+                    {
+                      rules: [{
+                        required: true,
+                        pattern: /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{10,}$/,
+                        message: '비밀번호를 입력하세요',
+                      }, {
+                        validator: validateToNextPassword,
+                        message: '비밀번호가 일치하지 않아요',
+                      }]
+                    }
+                  ]"
                 type="password"
+              />
+            </a-form-item>
+            <a-form-item
+              hasFeedback
+            >
+              <a-input
                 placeholder="비밀번호 확인"
-                data-vv-as="password"
-                name="confirmPassword"
-                v-model="confirmPassword"
-                v-validate.initial="'required|confirmed:password'"
+                v-decorator="[
+                    'confirm',
+                    {
+                      rules: [{
+                        required: true,
+                        message: '비밀번호확인을 입력하세요',
+                      }, {
+                        validator: compareToFirstPassword,
+                        message: '비밀번호가 일치하지 않아요',
+                      }]
+                    }
+                  ]"
+                type="password"
+                @blur="handleConfirmBlur"
               />
-          </div>
-          <div class="input-item-wrap flex-container flex-between-sort flex-row" style="width: 100%;">
-            <div style="width: 100%;">
-              <a-input
-                placeholder="이메일"
-                name="email"
-                v-model="email"
-                v-validate.initial="{
-                  required: true,
-                  regex: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-                }"
-              />
+            </a-form-item>
+            <div class="flex-container flex-between-sort flex-row">
+              <a-form-item
+                hasFeedback
+              >
+                <a-input
+                  placeholder="이메일입력"
+                  v-model="email"
+                  v-decorator="[
+                      'email',
+                      {
+                        rules: [{
+                            required: true,
+                            message: '이메일을 입력해주세요!',
+                          }, {
+                            type: 'email',
+                            message: '이메일형식이 아니에요',
+                            pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                          }]
+                      }
+                    ]"
+                />
+              </a-form-item>
+              <div class="button-wrap">
+                <a-button type="default" style="font-size: 10px;"
+                          @click="certificate()">인증</a-button>
+              </div>
             </div>
-            <div class="button-wrap">
-              <a-button type="default" style="font-size: 10px;"
-                        @click="certificate()">인증</a-button>
+            <div class="flex-container flex-between-sort flex-row" style="margin-bottom: 20px;">
+              <div>
+                <a-input
+                  v-model="token"
+                  placeholder="보안코드입력"
+                  type="text"
+                />
+              </div>
+              <div>
+                <a-button type="default" style="font-size: 10px;"
+                          @click="confirmToken()">확인</a-button>
+              </div>
             </div>
-          </div>
-          <div class="input-item-wrap flex-container flex-between-sort flex-row" style="width: 100%;">
-            <div style="width: 100%;">
-              <a-input
-                type="text"
-                placeholder="보안코드입력"
-                name="certified"
-                v-model="token"
-                v-validate.initial="{ required: true }"
-              />
-            </div>
-            <div class="button-wrap">
-              <a-button type="default" style="font-size: 10px;"
-                        @click="confirmToken()">확인</a-button>
-            </div>
-          </div>
+            <a-form-item>
+              <a-button type="primary" htmlType="submit" block>회원가입</a-button>
+            </a-form-item>
+          </a-form>
+          <a-button type="dashed" block style="width: 200px;">뒤로</a-button>
         </div>
-      <div class="navigation-box flex-container flex-column flex-between-sort">
-        <a-button type="primary" block @click="joinUser()">회원가입</a-button>
-        <a-button type="dashed" block>뒤로</a-button>
       </div>
     </div>
   </div>
@@ -96,22 +141,11 @@
 <script>
 export default {
   name: 'JoinPage',
-  computed: {
-    isTypedLabel() {
-      return {  // 개별적이지 않음. 디렉티브를 만들어야하나 고려
-        'typed-label': this.realName.length !== 0,
-        'valid': !this.errors.has('realName'),
-        'invalid': this.errors.has('realName'),
-      };
-    },
+  beforeCreate() {
+    this.form = this.$form.createForm(this);
   },
   data() {
     return {
-      realNamePlaceholder: '이름',
-      realName: '',
-      nickName: '',
-      password: '',
-      confirmPassword: '',
       email: '',
       token: '',
       certified: false,
@@ -119,19 +153,6 @@ export default {
     };
   },
   methods: {
-    joinUser() {
-      if (!this.errors.any()) {
-        this.$http.post('/api/join', {
-          realName: this.realName,
-          nickName: this.nickName,
-          password: this.password,
-          email: this.email,
-        })
-          .catch(err => console.error(err));
-      } else {
-        console.log('회원가입 실패임!');
-      }
-    },
     certificate() {
       this.$http.post('/api/cert/mail', {
         email: this.email,
@@ -147,7 +168,41 @@ export default {
       })
         .then(result => result.data)
         .catch(err => err);
-      console.log(this.certified);
+    },
+    handleSubmit(e) {
+      e.preventDefault();
+      this.form.validateFieldsAndScroll((err, values) => {
+        if (!err && !this.certified) { // 인증도 성공하고 폼도 맞다면 회원가입 진행
+          console.log('Recieved values of form: ', values);
+          /**
+           * api 인터페이스 작성
+           */
+        } else {
+          // 에러 존재시
+          console.log('err', err);
+          console.log('ciertified', this.certified);
+        }
+      });
+    },
+
+    validateToNextPassword(rule, value, callback) {
+      const form = this.form;
+      if (value && this.confirmDirty) {
+        form.validateFields(['confirm'], { force: true }, err => err);
+      }
+      callback();
+    },
+    compareToFirstPassword(rule, value, callback) {
+      const form = this.form;
+      if (value && value !== form.getFieldValue('password')) {
+        callback('비밀번호가 다릅니다.');
+      } else {
+        callback();
+      }
+    },
+    handleConfirmBlur(e) {
+      const value = e.target.value;
+      this.confirmDirty = this.confirmDirty || !!value;
     },
   },
 };
@@ -157,38 +212,15 @@ export default {
   @import './../assets/scss/mixin/typography';
   @import './../assets/scss/variables/colors';
 
-  .label--base {
-    color: #999;
-    height: 40px;
-    line-height: 50px;
-    white-space: nowrap;
-    font-size:  12px;
-    left: 13px;
-    z-index: 99;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    position: absolute;
-    user-select: none;
-    pointer-events: none;
-    transition: transform ease-out .1s, -webkit-transform ease-out .1s;
+  form {
+    width: 80%;
   }
-
-  .typed-label {
-    transform: scale(.7333) translateX(-20%) translateY(-15px);
-  }
-  .valid { color: #1DA1F2 !important; }
-  .invalid { color: #ff2128 !important; }
-  .error { color: #ff2128 !important; }
-
-  input {
-    padding-top: 14px;
+  input, input::placeholder {
     font-size: 10px;
-    height: 40px;
   }
 
   .join-box {
     width: 100%;
-    max-width: 400px;
     height: 100%;
     padding: 20px;
 
@@ -200,26 +232,30 @@ export default {
 
     .input-box {
       margin: 0 auto;
-      max-width: 70%;
-      min-height: 60%;
-      width: 70%;
+      max-width: 90%;
+      min-height: 70%;
+      width: 100%;
       height: auto;
 
       .input-item-wrap {
-        position: relative;
-        width: 100%;
-        height: 50px;
+        width: 250px;
 
         .button-wrap {
-          margin-left: 10px;
+          padding-top: 5px;
+          display: flex;
+          height: 63px;
         }
       }
     }
 
     .navigation-box {
-      margin: 15px auto;
-      width: 196px;
-      height: 80px;
+      margin-top: 10px;
+      width: 100%;
+      height: 70px;
+
+      .button-common {
+        min-width: 200px;
+      }
     }
 
   }

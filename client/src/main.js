fd441b1e5047b2c1656a041d863870296f919856
heecheol <msn9975@gmail.com>
2019-02-06 21:50:49 +0900

    test(client): geolocation, geocoding api 테스트

--- client/src/main.js
+++ client/src/main.js
@@ -5,6 +5,8 @@ import 'ant-design-vue/dist/antd.css';
 import Antd from 'ant-design-vue';
 import VeeValidate from 'vee-validate';
 import Vue from 'vue';
+import axios from 'axios';
+import VueGeolocation from 'vue-browser-geolocation';
 
 import router from './router';
 import api from './api/ApiService';
@@ -15,15 +17,17 @@ import './assets/scss/main.scss';
 
 Vue.config.productionTip = false;
 Vue.prototype.$api = api;
+Vue.prototype.$http = axios;
 
 Vue.use(VeeValidate);
 Vue.use(Antd);
+Vue.use(VueGeolocation);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>',
   store,
 });
--- client/src/pages/MainPage.vue
+++ client/src/pages/MainPage.vue
@@ -1,11 +1,41 @@
 <template>
   <div>
     main page
+    <a-button type="primary" @click="getLocation()">get Location</a-button>
+    <a-button type="dashed" @click="parseLocation()">Parse Location</a-button>
+
+    <pre>
+      {{ JSON.stringify(location) }}
+    </pre>
   </div>
 </template>
 
 <script>
 export default {
   name: 'MainPage',
+  data() {
+    return {
+      baseUrl: 'https://www.googleapis.com/geolocation/v1/geolocate?key=',
+      location: '',
+    };
+  },
+  methods: {
+    getLocation() {
+      /*this.$http.post(this.baseUrl + this.key)
+        .then(result => console.log('result', result))
+        .catch(err => console.error('err', err));*/
+    },
+    parseLocation() {
+      /*const lat = '37.6689979';
+      const lng = '127.05075719999999';
+      const url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=`;
+      this.$http.get(url)
+        .then(result => {
+          console.log(result);
+          this.location = result.data;
+        })
+        .catch(err => console.log('err', err));*/
+    },
+  },
 };
 </script>

c941b6124ca9ba88fc2ebe12c1ee092f65ea35c9
ahkong <devahgkong@google.com>
2019-02-19 01:59:16 +0900

    feat(server): 게시물 불러오기

---
+++ server/service/findId.js
@@ -0,0 +1,12 @@
+const knex = require('./service.config');
+
+module.exports = {
+  findId: async ({ realName, email }) => {
+    const result = await knex('users')
+      .select('realName', 'email')
+      .where({ realName, email })
+      .then(results => results)
+      .catch(err => err);
+    return result.length === 0; // 검색 되는게 없으면 해당 이메일과 이름으로 가입한 유저가 없는 것임
+  },
+};

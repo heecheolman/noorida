8913eb5fd9907a74592fe4220e2a09055a66e1ce
alswl1011 <whalswl576@naver.com>
2019-03-02 00:23:51 +0900

    client: 비밀번호찾기2-수정

--- client/src/pages/FindPasswordPage.vue
+++ client/src/pages/FindPasswordPage.vue
@@ -117,7 +117,6 @@ export default {
     },
   },
 };
-
 </script>
 
 <style lang="scss" scoped>
--- client/src/store/modules/find/find.js
+++ client/src/store/modules/find/find.js
@@ -3,47 +3,38 @@ import api from '@/api/ApiService';
 
 const state = {
   foundNickName: '',
-
   findPasswordSuccess: false,
-
 };
 
 const getters = {
-  getFindNickname : state => state.foundNickName,
-
-  getFindPasswordSuccess : state => state.findPasswordSuccess,
-
+  getFindNickname: state => state.foundNickName,
+  getFindPasswordSuccess: state => state.findPasswordSuccess,
 };
 const actions = {
-
-  findPasswordProcess : async ({commit}, payload) => {
+  findPasswordProcess: async ({commit}, payload) => {
     const { realName, nickName, email} = payload;
     const data = await api.findPassword(realName, nickName, email)
       .then(result => result.data)
       .catch(err=>err);
 
-    if(data.success){
+    if(data.success) {
       commit(types.FIND_PASSWORD_SUCCESS, true);
-
-
-    }else{
+    }else {
       commit(types.FIND_PASSWORD_SUCCESS, false);
-
     }
   },
-  findIdProcess : async ({ commit } , payload) => {
+  findIdProcess: async ({ commit } , payload) => {
     const { realName, email } = payload;
     const data =  await api.findId(realName , email)
       .then(result => result.data)
       .catch(err => err);
-    if (data && !(data instanceof Error) ) {
+    if (data && !(data instanceof Error)) {
       commit(types.SET_FOUND_ID, data.nickName);
     } else {
       commit(types.SET_FOUND_ID, null);
     }
   },
 };
-
 
 const mutations = {
   [types.SET_FOUND_ID] (state, payload) {

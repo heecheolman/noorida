cdb0c4f59b384c956649d38a6c75f1190e95a9ca
rkdalswl97 <kmj970610@gmail.com>
2019-03-04 20:31:48 +0900

    비밀번호 변경, 메일 전송 api 작성

--- server/service/findPassword.js
+++ server/service/findPassword.js
@@ -8,4 +8,14 @@ module.exports = {
       .catch(err => err);
     return result; // 검색 되는게 없으면 해당 이메일과 이름으로 가입한 유저가 없는 것임
   },
+
+  changePassword: async ({email, tmpPassword}) => {
+    const result = await knex('users')
+        .where({ email })
+        .update({password: tmpPassword} )
+        .then(results => results)
+        .catch(err => err);
+    return result; // 검색 되는게 없으면 해당 이메일과 이름으로 가입한 유저가 없는 것임
+  },
 };
+

29733e0758d2b7654bb629bd2c9301469c92f45e
ahkong <devahgkong@google.com>
2019-03-10 22:09:48 +0900

    feat(server): 구독 API

--- server/route/index.js
+++ server/route/index.js
@@ -9,6 +9,7 @@ const uploadRouter = require('./upload');
 const usersRouter = require('./users');
 const commentsRouter = require('./comment');
 const changePasswordRouter = require('./changePassword');
+const subscriptionRotuer = require('./subsription');
 
 
 router.use('/posts', postsRouter); // ok
@@ -18,6 +19,7 @@ router.use('/auth', authRouter); // not ok (find id, password)
 router.use('/users', usersRouter); // ok
 router.use('/comments', commentsRouter);
 router.use('/change-Password', changePasswordRouter);
+router.use('/subscription', subscriptionRotuer);
 
 
 module.exports = router;

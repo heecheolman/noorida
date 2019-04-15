b9cefd7988e9dc61b0961c63ac9a2ee1da0dff72
ahkong <devahgkong@google.com>
2019-04-15 23:51:25 +0900

    commit

--- client/src/pages/SearchAreaPage.vue
+++ client/src/pages/SearchAreaPage.vue
@@ -1,128 +1,105 @@
 <template>
   <div>
     <toolbar :title="'검색'" />
-    <div class="margin--10">
+    <div class="">
       <div class="design-card text-center">
+        <!--{{ LOCAL_NAME }}-->
         {{ localNameTag }}
       </div>
       <div class="button-common">
-        <a-button v-show="!isSubscribeLocal"
-                  type="primary"
-                  size="default"
-                  @click="subscribeLocal">구독하기
-        </a-button>
-        <a-button v-show="isSubscribeLocal"
-                  size="default"
-                  @click="cancelSubscribeLocal">구독중
-        </a-button>
+        <a-button >구독</a-button>
       </div>
+
+
+
+      <div class="margin--10">
+        <virtual-list :post-list="previewPostList"
+                      :load-type="'local'"/>
+      </div>
+
+
       <a-list :loading="loading">
         <div class="padding--10">
-          <a-list-item v-for="item in searchLocalPostList.result"
-                       :key="item.contentId" class="box-size"
+          <a-list-item v-for="item in searchPostList" :key="item.contentId" class="box-size"
                        @click="routeDetailPage(item.contentId)">
             <a-list-item-meta>
               <a slot="title" class="title">{{ item.title }}</a>
-              <span slot="description" class="title-nickname">{{ item.nickName }}</span>
+              <span slot="description" class="title-nickname">{{ item.nickName || '지역이름' }}</span>
               <a-avatar v-if="item.avatar" slot="avatar" :src="`http://localhost:3000/images/${item.avatar}`"/>
               <a-avatar v-else slot="avatar" icon="user"></a-avatar>
             </a-list-item-meta>
           </a-list-item>
         </div>
       </a-list>
+
+
     </div>
+
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-
-const Toolbar = () => import('@/components/Toolbar');
-
-
-export default {
-  name: 'SearchAreaPage',
-  components: {
-    Toolbar,
-  },
-  props: {
-    localId: {
-      type: Number,
+  import { mapState, mapMutations } from 'vuex';
+  const Toolbar = () => import('@/components/Toolbar');
+  const VirtualList = () => import('@/components/VirtualList');
+  export default {
+    name: 'SearchAreaPage',
+    components: {
+      Toolbar,
+      VirtualList,
     },
-    localName: {
-      type: String,
+    props: {
+      localId: {
+        type: Number,
+      },
+      localName: {
+        type: String,
+      },
     },
-    userId: {
-      type: Number,
+    computed: {
+      ...mapState('post', [
+        'previewPostList',
+      ]),
+      ...mapState('search', [
+        'loadLocalData',
+        'localList',
+        'searchPostList',
+      ]),
     },
-  },
-  computed: {
-    ...mapState('search', [
-      'searchLocalPostList',
-      'loading',
-    ]),
-    ...mapState('anotherUser', [
-      'isSubscribeLocal',
-    ]),
-  },
-  data() {
-    return {
-      address: this.$store.state.user.address,
-      localNameTag: '',
-      myUserId: this.$store.state.user.user.userId,
-    };
-  },
-  async created() {
-    this.initIsSubscribeLocal();
-    this.localNameTag = this.localName.substring(this.localName.lastIndexOf(' '));
-    await this.$store.dispatch('search/loadSearchLocalPost', {
-      localId: this.localId,
-      userId: this.myUserId,
-    });
-  },
-  methods: {
-    async initIsSubscribeLocal() {
-      await this.$store.dispatch('anotherUser/isSubscribeLocal', {
-        reader: this.myUserId,
-        localId: this.localId,
+    data() {
+      return {
+        loading: true,
+        address: this.$store.state.user.address,
+        localNameTag: '',
+      };
+    },
+    async created() {
+      this.localNameTag = this.localName.substring(this.localName.length - 4); // 이거 고칠필요 있음!!!
+      await this.$store.dispatch('search/loadSearchPost', {
+        word: this.localName,
       });
     },
-    async subscribeLocal() {
-      await this.$store.dispatch('anotherUser/subscribeLocal', {
-        reader: this.myUserId,
-        localId: this.localId,
-      });
+    methods: {
+      ...mapMutations('post', {
+        initPreviewList: 'INIT_PREVIEW_LIST',
+        initDetailPost: 'INIT_DETAIL_POST',
+        initProfileCard: 'INIT_PROFILE_CARD',
+      }),
     },
-    async cancelSubscribeLocal() {
-      await this.$store.dispatch('anotherUser/cancelSubscribeLocal', {
-        reader: this.myUserId,
-        localId: this.localId,
-      });
-    },
-    routeDetailPage(contentId) {
-      if (contentId) {
-        this.$router.push({
-          name: 'PostDetailPage',
-          params: { contentId },
-        });
-      }
-    },
-  },
-};
+  };
 </script>
 
 <style scoped>
-
   .margin--10{
     margin: 15px;
   }
   .design-card{
-    margin: 60px auto 20px auto;
-    width: 110px;
+    margin: 60px auto 25px auto;
+    width: 130px;
     background-color: #6299ff;
     border-radius: 100px;
-    height: 110px;
-    font-size: 26px;
+    height: 130px;
+    font-size: 30px;
     color: white;
     line-height: 4.3;
   }
--- client/src/pages/tabs-search/reporter-search-tab/ReporterSearchTab.vue
+++ client/src/pages/tabs-search/reporter-search-tab/ReporterSearchTab.vue
@@ -1,105 +1,120 @@
 <template>
   <a-list class="padding--10">
+    <!--<a-list-item :key="data.id" v-for="data in reportUserList">-->
     <a-list-item v-for="users in reportUserList" :key="users.userId" >
       <div @click="routeUserProfile(users.userId)">
-        <a-avatar v-if="users.avatar" :src="`http://localhost:3000/images/${users.avatar}`"></a-avatar>
+        <a-avatar v-if="profileCard.avatar" :src="`http://localhost:3000/images/${profileCard.avatar}`"></a-avatar>
         <a-avatar v-else icon="user"></a-avatar>
+        <!--<span  class="text-center">{{ data.reportNickname }}</span>-->
         <span  class="text-center design-name">{{ users.nickName }}</span>
       </div>
       <div class="button-common" >
-        <div v-if="users.userId !== myUserID">
-          <a-button v-show="!isSubscribe(users.userId)"
+        <div v-if="!isMe">
+          <a-button v-show="!isSubscribe"
                     type="primary"
-                    size="default"
-                    @click="subscribeReporter(users.userId)">구독하기
+                    size="small"
+                    @click="subscribeReporter">구독하기
           </a-button>
-          <a-button v-show="isSubscribe(users.userId)"
-                    size="default"
-                    @click="cancelSubscribeReporter(users.userId)">구독중
+          <a-button v-show="isSubscribe"
+                    size="small"
+                    @click="cancelSubscribeReporter">구독중
           </a-button>
         </div>
       </div>
+      <!--</div>-->
     </a-list-item>
   </a-list>
-
 </template>
 
 <script>
-import { mapState } from 'vuex';
-
-export default {
-  name: 'ReporterSearchTab',
-  data() {
-    return {
-      myUserID: this.$store.state.user.user.userId,
-    };
-  },
-  computed: {
-    ...mapState('user', [
-      'user',
-    ]),
-    ...mapState('post', [
-      'previewPostList',
-      'profileCard',
-    ]),
-    ...mapState('anotherUser', [
-      'info',
-      'reporterList',
-    ]),
-    ...mapState('search', [
-      'reportUserList',
-    ]),
-  },
-  created() {
-    this.initSubscribeUser();
-  },
-  methods: {
-    async initSubscribeUser() {
-      const userId = this.user.userId;
-      await this.$store.dispatch('anotherUser/fetchSubscribeList', {
-        fetchType: 'reporters',userId,
-      });
-    },
-    isSubscribe(userId) {
-      for (let i = 0; i < this.reporterList.length; i++) {
-        if (this.reporterList[i].userId === userId) {
-          return true;
-        }
-      }
-      return false;
-    },
-    async subscribeReporter(paramUserId) {
-      const payload = {
-        me: this.user.userId,
-        another: paramUserId,
+  import { mapState } from 'vuex';
+  import AListItem from 'ant-design-vue/es/list/Item';
+  const VirtualList = () => import('@/components/VirtualList');
+  // const contents = () => import('@/pages/tabs-search/local-search-tab');
+  export default {
+    name: 'ReporterSearchTab',
+    data() {
+      return {
+        // profileCard: this.$store.state.post.profileCard,
+        // inputData: this.$store.state.search.contents,
+        // address: this.$store.state.search.localName,
+        // // userName: this.$store.state.search.userName,
+        // mockList: [
+        //   { id: 1, mockNickname: 'joeminji' },
+        //   { id: 2, mockNickname: 'joyunji' },
+        //   { id: 3, mockNickname: 'hahahahaha' },
+        // ],
       };
-      await this.$store.dispatch('anotherUser/subscribeReporter', payload);
-      const userId = this.user.userId;
-      await this.$store.dispatch('anotherUser/fetchSubscribeList', {
-        fetchType: 'reporters', userId,
-      });
     },
-    async cancelSubscribeReporter(paramUserId) {
-      const payload = {
-        me: this.user.userId,
-        another: paramUserId,
-      };
-      await this.$store.dispatch('anotherUser/cancelSubscribeReporter', payload);
-      const userId = this.user.userId;
-      await this.$store.dispatch('anotherUser/fetchSubscribeList', {
-        fetchType: 'reporters', userId,
-      });
+    created() {
+      // this.initPreviewList();
+      // this.initDetailPost();
+      // this.initProfileCard();
+      // initPreviewPostList.nickName = this.userName;
+      console.log('모르는 아이디', this.$store.state.user.user.userId);
     },
-    async routeUserProfile(itemUserId) {
-      if (itemUserId) {
-        this.$router.push({
-          name: 'ProfilePage',
-          params: { userId: itemUserId },
+    components: {
+      AListItem,
+      PreviewPost,
+      VirtualList,
+      // contents,
+    },
+    computed: {
+      ...mapState('post', [
+        'previewPostList',
+        'profileCard',
+      ]),
+      ...mapState('anotherUser', [
+        'info',
+        'localList',
+        'isSubscribe',
+      ]),
+      ...mapState('search', [
+        'reportUserList',
+      ]),
+      isMe() {
+        return this.info.userId === this.$store.state.user.user.userId;
+      },
+    },
+    methods: {
+      // ...mapMutations('post', {
+      //   initPreviewList: 'INIT_PREVIEW_LIST',
+      //   initDetailPost: 'INIT_DETAIL_POST',
+      //   initProfileCard: 'INIT_PROFILE_CARD',
+      // }),
+      async subscribeReporter() {
+        const payload = {
+          me: this.$store.state.user.user.userId,
+          another: this.info.userId,
+        };
+        await this.$store.dispatch('anotherUser/subscribeReporter', payload);
+        const userId = this.isMe ? this.user.userId : this.info.userId;
+        await this.$store.dispatch('anotherUser/fetchSubscribeList', {
+          fetchType: 'readers', userId,
         });
-      }
+      },
+      async cancelSubscribeReporter() {
+        const payload = {
+          me: this.$store.state.user.user.userId,
+          another: this.info.userId,
+        };
+        await this.$store.dispatch('anotherUser/cancelSubscribeReporter', payload);
+        const userId = this.isMe ? this.user.userId : this.info.userId;
+        await this.$store.dispatch('anotherUser/fetchSubscribeList', {
+          fetchType: 'readers', userId,
+        });
+      },
+      async routeUserProfile(itemUserId) {
+        if (itemUserId) {
+          this.$router.push({
+            name: 'ProfilePage',
+            params: { userId: itemUserId },
+          });
+        }
+        console.log('프로필 페이지로 이동할것');
+      },
     },
-  },
-};
+  };
 </script>
 
 <style lang="scss" scoped>
@@ -111,6 +126,7 @@ export default {
   }
   .design-icon{
     font-size: 30px;
+    /* margin-right: 10px; */
     padding-left: unset;
     margin-right: 20px;
   }
